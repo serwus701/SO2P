573bea1d8cd3c292dc65e4ecf1c6a1b51ced9dd9
Michał Malewicz <75280214+serwus701@users.noreply.github.com>
2023-05-30 09:54:56 +0200

    notes added

--- main.cpp
+++ main.cpp
@@ -4,3 +4,12 @@ int main() {
     std::cout << "Initial commit. Might not work." << std::endl;
     return 0;
 }
+
+//male i duże auta
+//semafor - paczkowanie samochodów
+//czas przejazdu to pewna liczna iteracji badz losowo
+//sprawdzac czy samochód zszedl z semafora i jak tak to wpuszczac
+
+//maciez czasow przejazdu mostow
+
+
